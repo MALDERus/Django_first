99e2a6ab3e107e0c14352b3e30bdff45cb0bd8c5
MALDERus <ssh7@bk.ru>
2021-09-03 18:32:44 +0300

    Домашнее задание 2 урок

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,10 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'магазин'
+    }
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'каталог'
+    }
+    return render(request, 'mainapp/products.html', context)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': 'контакты'
+    }
+    return render(request, 'mainapp/contact.html', context)
+
+def temp(request):
+    return render(request, 'mainapp/temp1.html', {'data': 10})
