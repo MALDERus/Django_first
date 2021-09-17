4fde405ca72ffe603f2c7559b8472108a7c92ba1
MALDERus <ssh7@bk.ru>
2021-09-17 19:10:52 +0300

    Решение 6 ДЗ

--- geekshop/mainapp/management/commands/fill_db.py
+++ geekshop/mainapp/management/commands/fill_db.py
@@ -4,7 +4,6 @@ import os
 from django.core.management.base import BaseCommand
 from mainapp.models import ProductCategory, Product
 
-from authapp.models import ShopUser
 
 JSON_PATH = 'mainapp/json'
 
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,32 +1,76 @@
-from django.shortcuts import render
-from mainapp.models import Product, ProductCategory
-from django.shortcuts import get_object_or_404
-from basketapp.models import Basket
+import os
+import random
+import datetime
+import json
 
+from basketapp.models import Basket
+from django.shortcuts import render, get_object_or_404
+from mainapp.models import Product, ProductCategory
+
+# JSON_PATH = 'mainapp/json'
 
 # Create your views here.
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category). \
+                        exclude(pk=hot_product.pk)[:3]
+
+    return same_products
+
+# def load_from_json(file_name):
+#     with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as infile:
+#         return json.load(infile)
 
 def main(request):
     title = 'Главная'
-    content = {'title': title}
+    products = Product.objects.all()[:3]
+
+    content = {
+        'title': title,
+        'products': products,
+        'basket': get_basket(request.user),
+    }
+
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request, pk=None):
-    print(pk)
+def product(request, pk):
+    title = 'продукт'
 
+    content = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product.html', content)
+
+def products(request, pk=None):
     title = 'продукты'
     links_menu = ProductCategory.objects.all()
+    basket = get_basket(request.user)
 
-    basket = []
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
 
     if pk:
-        if pk == '0':
-            products = Product.objects.all().order_by('price')
+        if pk == 0:
             category = {'name': 'все'}
+            products = Product.objects.all().order_by('price')
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk).order_by('price')
@@ -41,16 +85,30 @@ def products(request, pk=None):
 
         return render(request, 'mainapp/products_list.html', content)
 
-    same_products = Product.objects.all()[3:5]
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
 
     content = {
         'title': title,
         'links_menu': links_menu,
-        'same_products': same_products
+        'hot_product': hot_product,
+        'same_products': same_products,
+        'basket': basket,
     }
 
     return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html', {'title': 'контакты'})
+    title = 'о нас'
+    visit_date = datetime.datetime.now()
+
+    # locations = load_from_json('contact_locations')
+
+    content = {
+        'title': title,
+        'visit_date': visit_date,
+        # 'locations': locations,
+        'basket': get_basket(request.user),
+    }
+    return render(request, 'mainapp/contact.html', content)
